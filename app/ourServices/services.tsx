fbd187c835de2288b50dcf66a6454aa9da0424e7
Norayr-10 <norayrt2004@gmail.com>
2025-02-12 07:04:10 +0400

    Our services folder done

---
+++ app/ourServices/services.tsx
@@ -0,0 +1,331 @@
+import { FC } from "react";
+import style from "./services.module.css";
+
+const Services: FC = () => {
+  return (
+    <div className={style["container"]}>
+      <div className={style["title"]}>
+        <h2>Services We Offer</h2>
+      </div>
+      <div className={style["singlr_content"]}>
+      <div className={style["content"]}>
+        <div className={style["ofered_services"]}>
+          <div className={style["logo_container"]}>
+            <img src="Frame 289308 (4).png" alt="" />
+          </div>
+          <div className={style["logo_Name"]}>
+            <h4>Web Developmet</h4>
+          </div>
+        </div>
+        <div className={style["ofered_services"]}>
+          <div className={style["logo_container"]}>
+            <img src="Frame 289308 (3).png" alt="" />
+          </div>
+          <div className={style["logo_Name"]}>
+            <h4>Mobile App Development</h4>
+          </div>
+        </div>
+        <div className={style["ofered_services"]}>
+          <div className={style["logo_container"]}>
+            <img src="Vector (1).png" alt="" />
+          </div>
+          <div className={style["logo_Name"]}>
+            <h4>DevOps</h4>
+          </div>
+        </div>
+        <div className={style["ofered_services"]}>
+          <div className={style["logo_container"]}>
+            <img src="Frame 289308.png" alt="" />
+          </div>
+          <div className={style["logo_Name"]}>
+            <h4>AI Integration</h4>
+          </div>
+        </div>
+      </div>
+      <div className={style["content"]}>
+        <div className={style["ofered_services"]}>
+          <div className={style["logo_container"]}>
+            <img src="Frame 289308 (5).png" alt="" />
+          </div>
+          <div className={style["logo_Name"]}>
+            <h4>Machine Learning</h4>
+          </div>
+        </div>
+        <div className={style["ofered_services"]}>
+          <div className={style["logo_container"]}>
+            <img src="Vector.png" alt="" />
+          </div>
+          <div className={style["logo_Name"]}>
+            <h4>Quality Assurance</h4>
+          </div>
+        </div>
+        <div className={style["ofered_services"]}>
+          <div className={style["logo_container"]}>
+            <img src="Frame 289308 (2).png" alt="" />
+          </div>
+          <div className={style["logo_Name"]}>
+            <h4>Technical Assessment</h4>
+          </div>
+        </div>
+        <div className={style["ofered_services"]}>
+          <div className={style["logo_container"]}>
+            <img src="Frame 289308 (1).png" alt="" />
+          </div>
+          <div className={style["logo_Name"]}>
+            <h4> Support & Maintenance</h4>
+          </div>
+        </div>
+      </div>
+      <div className={style["content"]}>
+        <div className={style["ofered_services"]}>
+          <div className={style["logo_container"]}>
+            <img src="Vector (4).png" alt="" />
+          </div>
+          <div className={style["logo_Name"]}>
+            <h4>Project Management</h4>
+          </div>
+        </div>
+        <div className={style["ofered_services"]}>
+          <div className={style["logo_container"]}>
+            <img src="Vector (2).png" alt="" />
+          </div>
+          <div className={style["logo_Name"]}>
+            <h4>UI/UX Design</h4>
+          </div>
+        </div>
+        <div className={style["ofered_services"]}>
+          <div className={style["logo_container"]}>
+            <img src="Frame 289308 (7).png" alt="" />
+          </div>
+          <div className={style["logo_Name"]}>
+            <h4>Digital Marketing</h4>
+          </div>
+        </div>
+        <div className={style["ofered_services"]}>
+          <div className={style["logo_container"]}>
+            <img src="Frame 289308 (6).png" alt="" />
+          </div>
+          <div className={style["logo_Name"]}>
+            <h4>Outstaffing</h4>
+          </div>
+        </div>
+      </div>
+</div>
+      <div className={style["secound_content"]}>
+        <div className={style["ofered_services"]}>
+          <div className={style["logo_container"]}>
+            <img src="Frame 289308 (4).png" alt="" />
+          </div>
+          <div className={style["logo_Name"]}>
+            <h4>Web Developmet</h4>
+          </div>
+        </div>
+        <div className={style["ofered_services"]}>
+          <div className={style["logo_container"]}>
+            <img src="Frame 289308 (3).png" alt="" />
+          </div>
+          <div className={style["logo_Name"]}>
+            <h4>Mobile App Development</h4>
+          </div>
+        </div>
+        <div className={style["ofered_services"]}>
+          <div className={style["logo_container"]}>
+            <img src="Vector (1).png" alt="" />
+          </div>
+          <div className={style["logo_Name"]}>
+            <h4>DevOps</h4>
+          </div>
+        </div>
+      </div>
+      <div className={style["secound_content"]}>
+        <div className={style["ofered_services"]}>
+          <div className={style["logo_container"]}>
+            <img src="Frame 289308.png" alt="" />
+          </div>
+          <div className={style["logo_Name"]}>
+            <h4>AI Integration</h4>
+          </div>
+        </div>
+        <div className={style["ofered_services"]}>
+          <div className={style["logo_container"]}>
+            <img src="Frame 289308 (5).png" alt="" />
+          </div>
+          <div className={style["logo_Name"]}>
+            <h4>Machine Learning</h4>
+          </div>
+        </div>
+        <div className={style["ofered_services"]}>
+          <div className={style["logo_container"]}>
+            <img src="Vector.png" alt="" />
+          </div>
+          <div className={style["logo_Name"]}>
+            <h4>Quality Assurance</h4>
+          </div>
+        </div>
+      </div>
+      <div className={style["secound_content"]}>
+        <div className={style["ofered_services"]}>
+          <div className={style["logo_container"]}>
+            <img src="Frame 289308 (2).png" alt="" />
+          </div>
+          <div className={style["logo_Name"]}>
+            <h4>Technical Assessment</h4>
+          </div>
+        </div>
+        <div className={style["ofered_services"]}>
+          <div className={style["logo_container"]}>
+            <img src="Frame 289308 (1).png" alt="" />
+          </div>
+          <div className={style["logo_Name"]}>
+            <h4> Support & Maintenance</h4>
+          </div>
+        </div>
+        <div className={style["ofered_services"]}>
+          <div className={style["logo_container"]}>
+            <img src="Vector (4).png" alt="" />
+          </div>
+          <div className={style["logo_Name"]}>
+            <h4>Project Management</h4>
+          </div>
+        </div>
+      </div>
+      <div className={style["secound_content"]}>
+        <div className={style["ofered_services"]}>
+          <div className={style["logo_container"]}>
+            <img src="Vector (2).png" alt="" />
+          </div>
+          <div className={style["logo_Name"]}>
+            <h4>UI/UX Design</h4>
+          </div>
+        </div>
+        <div className={style["ofered_services"]}>
+          <div className={style["logo_container"]}>
+            <img src="Frame 289308 (7).png" alt="" />
+          </div>
+          <div className={style["logo_Name"]}>
+            <h4>Digital Marketing</h4>
+          </div>
+        </div>
+        <div className={style["ofered_services"]}>
+          <div className={style["logo_container"]}>
+            <img src="Frame 289308 (6).png" alt="" />
+          </div>
+          <div className={style["logo_Name"]}>
+            <h4>Outstaffing</h4>
+          </div>
+        </div>
+      </div>
+
+      <div className={style["third_container"]}>
+        <div className={style["third_content"]}>
+          <div className={style["ofered_services"]}>
+            <div className={style["logo_container"]}>
+              <img src="Frame 289308 (4).png" alt="" />
+            </div>
+            <div className={style["logo_Name"]}>
+              <h4>Web Developmet</h4>
+            </div>
+          </div>
+
+          <div className={style["ofered_services"]}>
+            <div className={style["logo_container"]}>
+              <img src="Vector (1).png" alt="" />
+            </div>
+            <div className={style["logo_Name"]}>
+              <h4>DevOps</h4>
+            </div>
+          </div>
+          <div className={style["ofered_services"]}>
+            <div className={style["logo_container"]}>
+              <img src="Frame 289308 (5).png" alt="" />
+            </div>
+            <div className={style["logo_Name"]}>
+              <h4>Machine Learning</h4>
+            </div>
+          </div>
+          <div className={style["ofered_services"]}>
+            <div className={style["logo_container"]}>
+              <img src="Frame 289308 (2).png" alt="" />
+            </div>
+            <div className={style["logo_Name"]}>
+              <h4>Technical Assessment</h4>
+            </div>
+          </div>
+          <div className={style["ofered_services"]}>
+            <div className={style["logo_container"]}>
+              <img src="Vector (4).png" alt="" />
+            </div>
+            <div className={style["logo_Name"]}>
+              <h4>Project Management</h4>
+            </div>
+          </div>
+          <div className={style["ofered_services"]}>
+            <div className={style["logo_container"]}>
+              <img src="Frame 289308 (7).png" alt="" />
+            </div>
+            <div className={style["logo_Name"]}>
+              <h4>Digital Marketing</h4>
+            </div>
+          </div>
+        </div>
+        <div className={style["third_content"]}>
+          <div className={style["ofered_services"]}>
+            <div className={style["logo_container"]}>
+              <img src="Frame 289308 (3).png" alt="" />
+            </div>
+            <div className={style["logo_Name"]}>
+              <h4>Mobile App Development</h4>
+            </div>
+          </div>
+
+          <div className={style["ofered_services"]}>
+            <div className={style["logo_container"]}>
+              <img src="Frame 289308.png" alt="" />
+            </div>
+            <div className={style["logo_Name"]}>
+              <h4>AI Integration</h4>
+            </div>
+          </div>
+
+          <div className={style["ofered_services"]}>
+            <div className={style["logo_container"]}>
+              <img src="Vector.png" alt="" />
+            </div>
+            <div className={style["logo_Name"]}>
+              <h4>Quality Assurance</h4>
+            </div>
+          </div>
+
+          <div className={style["ofered_services"]}>
+            <div className={style["logo_container"]}>
+              <img src="Frame 289308 (1).png" alt="" />
+            </div>
+            <div className={style["logo_Name"]}>
+              <h4> Support & Maintenance</h4>
+            </div>
+          </div>
+
+          <div className={style["ofered_services"]}>
+            <div className={style["logo_container"]}>
+              <img src="Vector (2).png" alt="" />
+            </div>
+            <div className={style["logo_Name"]}>
+              <h4>UI/UX Design</h4>
+            </div>
+          </div>
+
+          <div className={style["ofered_services"]}>
+            <div className={style["logo_container"]}>
+              <img src="Frame 289308 (6).png" alt="" />
+            </div>
+            <div className={style["logo_Name"]}>
+              <h4>Outstaffing</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./header/head";
 import Main from "./company/main";
+import Services from "./ourServices/services";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Header/>
       <Main/>
+      <Services/>
     </div>
   );
 }
